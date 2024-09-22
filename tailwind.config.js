3ed4212b6da90d657eb868f1edd6baca90c36979
Johannes Böhm <pulsone21@freenet.de>
2024-09-22 13:57:50 +0200

    UI Pipeline settup
    Added TailwindCSS and Templ as dependcies, added first tempaltes

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["internal/templates/*.templ"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        mobile: "2rem",
+        tablet: "4rem",
+        desktop: "5rem",
+      },
+    },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@catppuccin/tailwindcss"),
+  ],
+};
