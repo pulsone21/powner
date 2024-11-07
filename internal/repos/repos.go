e460bbfd46fcd9e889959882535c2d60cc03af09
Johannes Böhm <pulsone21@freenet.de>
2024-11-07 06:18:10 +0100

    ID Inputs in Repository Interface should be already sanitized

--- internal/repos/repos.go
+++ internal/repos/repos.go
@@ -4,28 +4,28 @@ import "github.com/pulsone21/powner/internal/entities"
 
 type TeamRepository interface {
 	GetAll() (*[]entities.Team, error)
-	GetByID(id string) (*entities.Team, error)
+	GetByID(id uint) (*entities.Team, error)
 	Create(newTeam entities.Team) (*entities.Team, error)
 	Update(newTeam entities.Team) (*entities.Team, error)
-	Delete(id string) error
-	RemoveMember(team_id string, mem entities.Member) error
-	AddMember(team_id string, mem entities.Member) error
-	AddSkill(team_id string, skill entities.Skill) error
-	RemoveSkill(team_id string, skill entities.Skill) error
+	Delete(id uint) error
+	RemoveMember(team_id uint, mem entities.Member) error
+	AddMember(team_id uint, mem entities.Member) error
+	AddSkill(team_id uint, skill entities.Skill) error
+	RemoveSkill(team_id uint, skill entities.Skill) error
 }
 
 type MemberRepository interface {
 	GetAll() (*[]entities.Member, error)
-	GetByID(id string) (*entities.Member, error)
+	GetByID(id uint) (*entities.Member, error)
 	Create(newTeam entities.Member) (*entities.Member, error)
 	Update(newTeam entities.Member) (*entities.Member, error)
-	Delete(id string) error
+	Delete(id uint) error
 }
 
 type SkillRepository interface {
 	GetAll() (*[]entities.Skill, error)
-	GetByID(id string) (*entities.Skill, error)
+	GetByID(id uint) (*entities.Skill, error)
 	Create(newTeam entities.Skill) (*entities.Skill, error)
 	Update(newTeam entities.Skill) (*entities.Skill, error)
-	Delete(id string) error
+	Delete(id uint) error
 }
