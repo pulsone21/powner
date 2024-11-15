38a000c1a4289742c49f858953e37c7ac7fed987
Johannes Böhm <pulsone21@freenet.de>
2024-11-15 22:17:18 +0100

    Depricating old API Package and refactored to use the new server package

--- internal/api/memberHandler.go
+++ internal/api/memberHandler.go
@@ -1,111 +1,112 @@
 package api
 
-import (
-	"net/http"
-	"strconv"
-
-	"github.com/pulsone21/powner/internal/entities"
-	"github.com/pulsone21/powner/internal/ui/modals"
-	"github.com/pulsone21/powner/internal/ui/partials"
-)
-
-type memberRequest struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
-
-func getMembers(w http.ResponseWriter, r *http.Request) *response {
-	mem, err := entities.GetMembers(db)
-	if err != nil {
-		return internalError(err)
-	}
-
-	if len(*mem) == 0 {
-		return newResponse(nil, nil, 200, nil)
-	}
-
-	// return success(mem, partials.MemberList(*mem, nil, "No member found"))
-	return success(mem, nil)
-}
-
-func createMember(w http.ResponseWriter, r *http.Request) *response {
-	memReq, err := decodeRequest[memberRequest](r)
-	if err != nil {
-		return badRequest(err)
-	}
-
-	mem, err := entities.CreateMember(db, *entities.NewMember(memReq.Name, memReq.Age))
-	if err != nil {
-		return internalError(err)
-	}
-
-	w.Header().Add("HX-Trigger", "newMember")
-	return newResponse(mem, partials.MemberForm(), 201, nil)
-}
-
-func getMemberById(w http.ResponseWriter, r *http.Request) *response {
-	strId := r.PathValue("id")
-	id, err := strconv.Atoi(strId)
-	if err != nil {
-		return idNotValid(strId)
-	}
-	mem, err := entities.GetMemberById(db, uint(id))
-	if err != nil {
-		return internalError(err)
-	}
-
-	if mem == nil {
-		return newResponse(nil, nil, 200, nil)
-	}
-
-	return success(mem, modals.MemberInfoModal(*mem))
-}
-
-func deleteMember(w http.ResponseWriter, r *http.Request) *response {
-	strId := r.PathValue("id")
-	id, err := strconv.Atoi(strId)
-	if err != nil {
-		return idNotValid(strId)
-	}
-	err = entities.DeleteMember(db, uint(id))
-	if err != nil {
-		return internalError(err)
-	}
-	return success("Done", nil)
-}
-
-func updateMember(w http.ResponseWriter, r *http.Request) *response {
-	memReq, err := decodeRequest[memberRequest](r)
-
-	strId := r.PathValue("id")
-	id, err := strconv.Atoi(strId)
-	if err != nil {
-		return idNotValid(strId)
-	}
-
-	newMem := &entities.Member{
-		Name: memReq.Name,
-		Age:  memReq.Age,
-	}
-
-	newMem.ID = uint(id)
-	err = entities.UpdateMember(db, *newMem)
-	if err != nil {
-		return internalError(err)
-	}
-
-	return success(newMem, nil)
-}
-
-func try_find_member(id uint) (*entities.Member, *response) {
-	mem, err := entities.GetMemberById(db, id)
-	if err != nil {
-		return nil, internalError(err)
-	}
-
-	if mem == nil {
-		return nil, idNotValid(string(id))
-	}
-
-	return mem, nil
-}
+//
+//import (
+//	"net/http"
+//	"strconv"
+//
+//	"github.com/pulsone21/powner/internal/entities"
+//	"github.com/pulsone21/powner/internal/ui/modals"
+//	"github.com/pulsone21/powner/internal/ui/partials"
+//)
+//
+//type memberRequest struct {
+//	Name string `json:"name"`
+//	Age  int    `json:"age"`
+//}
+//
+//func getMembers(w http.ResponseWriter, r *http.Request) *response {
+//	mem, err := entities.GetMembers(db)
+//	if err != nil {
+//		return internalError(err)
+//	}
+//
+//	if len(*mem) == 0 {
+//		return newResponse(nil, nil, 200, nil)
+//	}
+//
+//	// return success(mem, partials.MemberList(*mem, nil, "No member found"))
+//	return success(mem, nil)
+//}
+//
+//func createMember(w http.ResponseWriter, r *http.Request) *response {
+//	memReq, err := decodeRequest[memberRequest](r)
+//	if err != nil {
+//		return badRequest(err)
+//	}
+//
+//	mem, err := entities.CreateMember(db, *entities.NewMember(memReq.Name, memReq.Age))
+//	if err != nil {
+//		return internalError(err)
+//	}
+//
+//	w.Header().Add("HX-Trigger", "newMember")
+//	return newResponse(mem, partials.MemberForm(), 201, nil)
+//}
+//
+//func getMemberById(w http.ResponseWriter, r *http.Request) *response {
+//	strId := r.PathValue("id")
+//	id, err := strconv.Atoi(strId)
+//	if err != nil {
+//		return idNotValid(strId)
+//	}
+//	mem, err := entities.GetMemberById(db, uint(id))
+//	if err != nil {
+//		return internalError(err)
+//	}
+//
+//	if mem == nil {
+//		return newResponse(nil, nil, 200, nil)
+//	}
+//
+//	return success(mem, modals.MemberInfoModal(*mem))
+//}
+//
+//func deleteMember(w http.ResponseWriter, r *http.Request) *response {
+//	strId := r.PathValue("id")
+//	id, err := strconv.Atoi(strId)
+//	if err != nil {
+//		return idNotValid(strId)
+//	}
+//	err = entities.DeleteMember(db, uint(id))
+//	if err != nil {
+//		return internalError(err)
+//	}
+//	return success("Done", nil)
+//}
+//
+//func updateMember(w http.ResponseWriter, r *http.Request) *response {
+//	memReq, err := decodeRequest[memberRequest](r)
+//
+//	strId := r.PathValue("id")
+//	id, err := strconv.Atoi(strId)
+//	if err != nil {
+//		return idNotValid(strId)
+//	}
+//
+//	newMem := &entities.Member{
+//		Name: memReq.Name,
+//		Age:  memReq.Age,
+//	}
+//
+//	newMem.ID = uint(id)
+//	err = entities.UpdateMember(db, *newMem)
+//	if err != nil {
+//		return internalError(err)
+//	}
+//
+//	return success(newMem, nil)
+//}
+//
+//func try_find_member(id uint) (*entities.Member, *response) {
+//	mem, err := entities.GetMemberById(db, id)
+//	if err != nil {
+//		return nil, internalError(err)
+//	}
+//
+//	if mem == nil {
+//		return nil, idNotValid(string(id))
+//	}
+//
+//	return mem, nil
+//}
--- internal/api/response.go
+++ internal/api/response.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"encoding/json"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -49,41 +48,3 @@ func success(data any, html templ.Component) *response {
 }
 
 type responseFunc func(w http.ResponseWriter, r *http.Request) *response
-
-func (res *response) Respond(w http.ResponseWriter, r *http.Request) {
-	if res.Error != nil {
-		w.Header().Set("Content-Type", "application/json")
-		slog.Error(res.Error.Error())
-		w.WriteHeader(res.StatusCode)
-		w.Write([]byte(res.Error.Error()))
-		return
-	}
-
-	if r.Header.Get("Hx-Request") == "true" {
-		slog.Info("Request is an HTMX Request")
-
-		if res.Html == nil {
-			w.Header().Set("Content-Type", "application/json")
-			slog.Error("Not an htmx route\n")
-			w.WriteHeader(404)
-			w.Write([]byte("Not an htmx route"))
-			return
-		}
-		w.Header().Set("Content-Type", "text/html")
-		w.WriteHeader(res.StatusCode)
-		res.Html.Render(r.Context(), w)
-		return
-
-	} else {
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(res.StatusCode)
-		if res.Data == nil {
-			slog.Info("Nothing found for that request")
-			json.NewEncoder(w).Encode(&empty{})
-			return
-		}
-
-		json.NewEncoder(w).Encode(res.Data)
-	}
-}
