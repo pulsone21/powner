af502ba2a9a5e930a50736ad501b01468d777af9
Johannes Böhm <pulsone21@freenet.de>
2024-11-19 09:19:08 +0100

    removed unneeded comment

--- internal/server/response/response.go
+++ internal/server/response/response.go
@@ -1,6 +1,8 @@
 package response
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type IResponse interface {
 	Respond(http.ResponseWriter, *http.Request)
@@ -8,5 +10,4 @@ type IResponse interface {
 
 type empty struct{}
 
-// TODO: i should implement some kind of context here to give options for RBAC and better logging based on request ID and stuff like this
 type ResponseFunc func(w http.ResponseWriter, r *http.Request) IResponse
