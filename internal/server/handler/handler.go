8338ae7930b3aa88a30163a331529632ae2fde83
Johannes Böhm <pulsone21@freenet.de>
2024-12-24 14:33:53 +0100

    Added schema decoded for form data

--- internal/server/handler/handler.go
+++ internal/server/handler/handler.go
@@ -3,8 +3,11 @@ package handler
 import (
 	"net/http"
 
+	"github.com/gorilla/schema"
 	"github.com/pulsone21/powner/internal/server/response"
 )
+
+var decoder = schema.NewDecoder()
 
 func setupHandler(fn response.ResponseFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
