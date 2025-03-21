1bc87ca35f847c3035b43c59472c6cae9c969051
Johannes Böhm <pulsone21@freenet.de>
2025-03-21 22:56:46 +0100

    Reworking skillRating entitity

--- internal/entities/skillRating.go
+++ internal/entities/skillRating.go
@@ -1,14 +1,8 @@
 package entities
 
-import (
-	"errors"
-
-	"gorm.io/gorm"
-)
-
 type SkillRating struct {
-	gorm.Model
-	SkillID  int   `json:"skill_id"`
+	ID       uint
+	SkillID  uint  `json:"skill_id"`
 	Skill    Skill `json:"skill"`
 	MemberID uint  `json:"member_id"`
 	Rating   int   `json:"rating"`
@@ -16,33 +10,9 @@ type SkillRating struct {
 
 func NewSkillRating(mem_id uint, skill Skill) *SkillRating {
 	return &SkillRating{
-		SkillID:  int(skill.ID),
+		SkillID:  skill.ID,
 		Skill:    skill,
 		MemberID: mem_id,
-		Rating:   0,
+		Rating:   1,
 	}
-}
-
-func UpdateSkillRating(db *gorm.DB, ratId uint, rating int) error {
-	res := db.Model(SkillRating{}).Preload("Skill").Where("Id = ?", ratId).Update("rating", rating)
-	return res.Error
-}
-
-func GetSkillRating(db *gorm.DB, id uint) (*SkillRating, error) {
-	var rat SkillRating
-	res := db.Model(SkillRating{}).Preload("Skill").First(&rat, "Id = ?", id)
-
-	if errors.Is(res.Error, gorm.ErrRecordNotFound) {
-		return nil, nil
-	}
-
-	if res.Error != nil {
-		return nil, res.Error
-	}
-
-	return &rat, nil
-}
-
-func DeleteSkillRating(db *gorm.DB, id uint) error {
-	return db.Delete(&SkillRating{}, id).Error
 }
